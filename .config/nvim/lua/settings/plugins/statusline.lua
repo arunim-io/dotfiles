ed61fb0208fbce6acf4f70a8d68ca4f2742bd523
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-05-15 23:40:07 +0600

    update 15/05/2023

--- .config/nvim/lua/settings/plugins/statusline.lua
+++ .config/nvim/lua/settings/plugins/statusline.lua
@@ -1,30 +1,5 @@
 return { {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = true,
-  opts = {
-    options = {
-      theme = 'codedark',
-      sections = {
-        lualine_b = {
-          'branch', 'diff',
-          {
-            'diagnostics',
-            sources = { 'nvim_diagnostic', 'nvim_lsp' },
-            sections = { 'error', 'warn', 'info', 'hint' },
-            diagnostics_color = {
-              error = 'DiagnosticError',
-              warn  = 'DiagnosticWarn',
-              info  = 'DiagnosticInfo',
-              hint  = 'DiagnosticHint',
-            },
-            symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
-            colored = true,
-            update_in_insert = true,
-            always_visible = false,
-          }
-        },
-      },
-    },
-  },
+  config = function() require 'settings.configs.statusline' end,
 } }
