fee2beacd9ef98e0e070e732125700f277231890
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-06-17 01:40:59 +0600

    replace kitty with wezterm
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,55 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+-- Some defaults
+
+if wezterm.config_builder then config = wezterm.config_builder() end
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- wezterm.mux
+
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function()
+  local tab, pane, window = mux.spawn_window {}
+  window:gui_window():maximize()
+end)
+
+
+-- Keybindings
+
+local actions = wezterm.action
+
+config.keys = {
+  { key = 'p',   mods = 'CTRL|SHIFT', action = actions.ActivateCommandPalette },
+  { key = 'c',   mods = 'CTRL',       action = actions.CopyTo 'Clipboard' },
+  { key = 'p',   mods = 'CTRL',       action = actions.PasteFrom 'Clipboard' },
+  { key = 'F11', mods = 'NONE',       action = actions.ToggleFullScreen, },
+  { key = 'q',   mods = 'CTRL|SHIFT', action = actions.QuitApplication },
+}
+
+-- UI Customizations
+
+config.color_scheme = 'Vs Code Dark+ (Gogh)'
+
+config.window_frame = {
+  font = wezterm.font { family = 'Ubuntu' }
+}
+
+config.window_background_opacity = 0.75
+
+config.tab_bar_at_bottom = true
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.inactive_pane_hsb = { brightness = 0.5 }
+
+config.use_dead_keys = false
+
+config.adjust_window_size_when_changing_font_size = false
+
+config.scrollback_lines = 10000
+
+return config
