618785e4db5c8b60383b9c9ea6fc01349b0220d9
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-08-23 23:28:23 +0600

    home-manager: add packages for neovim shell-scripting
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- dot_config/home-manager/neovim.nix
+++ dot_config/home-manager/neovim.nix
@@ -2,7 +2,7 @@
 let
   neovim = inputs.neovim-nightly.packages.${pkgs.system}.neovim;
   packages = with pkgs; [
-    # djlint - handled with pipx
+    # handled by pipx - djlint sourcery
     tree-sitter
     fd
     ripgrep
@@ -24,6 +24,9 @@ let
     statix
     nodePackages.dockerfile-language-server-nodejs
     nodePackages."@tailwindcss/language-server"
+    nodePackages.bash-language-server
+    shfmt
+    shellcheck
   ];
 in
 {
